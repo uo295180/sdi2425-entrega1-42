ee717ee22b8fdb8e77b72eea44fb0d7dde0d3461
orvizz <marioorvvie@gmail.com>
2025-03-03 13:38:05 +0100

    Initial commit

---
+++ src/main/java/com/uniovi/sdi/sdi2425entrega142/Sdi2425Entrega142Application.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.sdi2425entrega142;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2425Entrega142Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2425Entrega142Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdi/sdi2425entrega142/Sdi2425Entrega142ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.sdi2425entrega142;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2425Entrega142ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
