e55b8de7fcee24ded7025d781f9aac6b637daa6d
orvizz <marioorvvie@gmail.com>
2025-03-06 09:04:50 +0100

    2425-412 - Creación de clases del modelo de dominio. Implementación de la entidad empleado

---
+++ src/main/java/com/uniovi/sdi/sdi2425entrega142/entities/Vehiculo.java
@@ -0,0 +1,14 @@
+package com.uniovi.sdi.sdi2425entrega142.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Vehiculos")
+public class Vehiculo {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+
+}
